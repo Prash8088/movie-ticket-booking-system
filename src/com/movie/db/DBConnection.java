daaa2f9dfb66b633a8810e1fec309bc3cd56aaa8
Prashanth <bprashantha0@gmail.com>
2026-05-17 12:57:37 +0530

    added initial java files

---
+++ src/com/movie/db/DBConnection.java
@@ -0,0 +1,5 @@
+package com.movie.db;
+
+public class DBConnection {
+
+}
---
+++ src/com/movie/ui/Main.java
@@ -0,0 +1,9 @@
+package com.movie.ui;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Movie Ticket Booking System");
+    }
+}
